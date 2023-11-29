0302e9776421c060889be7cfca3290c4e05746eb
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-29 13:22:24 +0000

    Task 3 and 4 commit

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Repetition token #2 ruby script
+puts ARGV[0].scan(/hbt+n/).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Repetition Token #3 ruby script
+puts ARGV[0].scan(/hbt*n/).join
