1f13f9712e5f23b4b771922860555304e4928d5f
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-29 13:33:13 +0000

    task 6 and 7 commit

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Call me maybe ruby script
+puts ARGV[0].scan(/^\d{10,10}$/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# OMG Why are you shouting ruby script
+puts ARGV[0].scan(/[A-Z]*/).join
